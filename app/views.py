946927f39c114c25e69e124bf585b86b1e05147f
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-09-21 00:26:52 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,34 +1,42 @@
-import json
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .utils import suggest_transit_options
-
-
-def index(request):
-    return render(request, 'app/hello_webpack.html')
+from django.http import HttpResponseBadRequest
 
 
 @csrf_exempt
 def get_transit_suggestions(request):
     if request.method == "POST":
-        data = json.loads(request.body)
-        start_latitude = float(data.get('start_latitude', 0))
-        start_longitude = float(data.get('start_longitude', 0))
-        end_latitude = float(data.get('end_latitude', 0))
-        end_longitude = float(data.get('end_longitude', 0))
-    else:  # If it's a GET request
-        start_latitude = float(request.GET.get('start_latitude', 0))
-        start_longitude = float(request.GET.get('start_longitude', 0))
-        end_latitude = float(request.GET.get('end_latitude', 0))
-        end_longitude = float(request.GET.get('end_longitude', 0))
+        data = extract_data_from_post_request(request)
+    elif request.method == "GET":
+        data = extract_data_from_get_request(request)
+    else:
+        return HttpResponseBadRequest("Invalid request method")
 
-    suggestion = suggest_transit_options(start_latitude, start_longitude, end_latitude, end_longitude)
+    if not data:
+        return HttpResponseBadRequest("Invalid or missing parameters")
+
+    suggestion = suggest_transit_options(data['start_geohash'], data['end_geohash'])
 
     return JsonResponse({'suggestion': suggestion})
 
-def get_all_destinations(request):
-    destinations = Destination.objects.all()
-    data = [destination.serialize() for destination in destinations]
-    return JsonResponse(data, safe=False)
+
+def extract_data_from_post_request(request):
+    try:
+        data = json.loads(request.body)
+        return {
+            'start_geohash': data.get('start_geohash'),
+            'end_geohash': data.get('end_geohash')
+        }
+    except (ValueError, KeyError, TypeError):
+        return None
+
+
+def extract_data_from_get_request(request):
+    try:
+        # Parsing the JSON string from the query parameter
+        data = json.loads(request.GET.get('data', '{}'))
+        return {
+            'start_geohash': data.get('start_geohash'),
+            'end_geohash': data.get('end_geohash')
+        }
+    except (ValueError, KeyError, TypeError):
+        return None
 
