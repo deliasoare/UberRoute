d7d65b633e8f36eabaf738242a65993f25832c78
deliasoare <deliasoare006@yahoo.com>
2023-10-15 10:44:49 +0300

    Finish sidebar design

--- assets/components/Sidebar.jsx
+++ assets/components/Sidebar.jsx
@@ -3,11 +3,13 @@ function Sidebar({forwardedRef, closeSidebar, history, setComputation}) {
     return (
         <div ref={forwardedRef} className="sidebar">
             <span className="closeSidebar" onClick={closeSidebar}>X</span>
-            <div className="upperPart"></div>
+            <div className="upperPart">
+                <p>History</p>
+            </div>
             <div className="lowerPart">
                 {history && history.map(route => {
                     return (
-                        <div onClick={() => {setComputation(route)}} className='routeHistory'>{route[2]} - {route[3]}</div>
+                        <div onClick={() => {setComputation(route)}} className='routeHistory'>*  {route[2]} - {route[3]}</div>
                     );
                 })}
             </div>
